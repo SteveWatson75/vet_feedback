74b26203eb81430e27aeea663e018ddb59801da6
Steven <88657039+Steven-lunchCo@users.noreply.github.com>
2023-07-16 13:58:12 +0200

    feat: Update screen with multichoice components

--- client/vet_feedback/src/Screens/SurveyScreen/SurveyScreen.tsx
+++ client/vet_feedback/src/Screens/SurveyScreen/SurveyScreen.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
-import { Header, TitleText } from '../../Style/styled'
+import React, { useState } from 'react'
+import MultiChoiceCheckBox from 'src/components/MultiChoiceCheckBox'
+import MultiChoiceRadio from 'src/components/MultiChoiceRadio'
+import { Header, TitleText } from '../../style/styled'
 
 const SurveyScreen: React.FC = () => {
+  const [selectedOptionsRadio, setSelectedOptionsRadio] = useState<string[]>([])
+  const [selectedOptionCheckBox, setSelectedOptionCheckBox] = useState<string>('')
+
+  const question = 'What you want?'
+
   return (
-    <Header>
-      <TitleText>Survey Screen</TitleText>
-    </Header>
+    <>
+      <Header>
+        <TitleText>Survey Screen</TitleText>
+      </Header>
+      <MultiChoiceRadio
+        question={question}
+        choices={['fun', 'happiness', 'love']}
+        setSelectedOptionsRadio={setSelectedOptionsRadio}
+      />
+      <MultiChoiceCheckBox
+        question={question}
+        choices={['fun', 'happiness', 'love']}
+        selectedOption={selectedOptionCheckBox}
+        setSelectedOption={setSelectedOptionCheckBox}
+      />
+    </>
   )
 }
 
